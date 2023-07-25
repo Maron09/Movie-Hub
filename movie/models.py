c17ea19fa47c17f617658a2969da92d42c7d0cf6
chimaroke onyebi <maroks216@gmail.com>
2023-07-25 21:06:32 +0100

    database config

--- movie/models.py
+++ movie/models.py
@@ -12,9 +12,19 @@ LANGUAGE_CHOICES= (
     ('GR', 'GERMAN'),
 )
 
+STATUS_CHOICES= (
+    ('RA', 'RECENTLY ADDED'),
+    ('MW', 'MOST WATCHED'),
+    ('TR', 'TOP RATED'),
+)
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length= 1000)
-    image = models.ImageField(upload='movies')
+    image = models.ImageField(upload_to='movies')
     category = models.CharField(choices=CATEGORY_CHIOCES, max_length=2)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
+    year = models.DateField()
+    views = models.IntegerField(default=0)
